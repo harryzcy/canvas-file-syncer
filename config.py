5de7cf119774d355983c58412228af2c7eb348c0
Harry Zheng <harry@harryzheng.com>
2022-02-17 14:24:38 -0500

    Support getting website configs

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
+from typing import Dict, List
 import yaml
 
 
 config = {}
 
 DEFAULT_CONFIG_FILE = "config.yaml"
+
 
 def init(filename: str | None = DEFAULT_CONFIG_FILE):
     """initialize configurations"""
@@ -14,6 +16,9 @@ def init(filename: str | None = DEFAULT_CONFIG_FILE):
     with open(filename, 'r') as stream:
         global config
         config = yaml.safe_load(stream)
+        if config is None:
+            print('failed to load the configuration file')
+            exit(1)
 
 
 def get_username():
@@ -30,3 +35,14 @@ def get_password():
         print('CANVAS_PASSWORD not specified')
         exit(1)
     return password
+
+
+def get_websites() -> List[str]:
+    return config.get('websites').keys()
+
+
+def get_website_config(website_name: str, field: str):
+    website = config['websites'][website_name]
+    if field in website:
+        return website[field]
+    return config['websites']['default'][field]
